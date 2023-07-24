b16d30bd1875bc603ee470ea883fcd403d2d901b
ShadowMikado <89030950+ShadowMikado@users.noreply.github.com>
2023-07-24 20:04:40 +0200

    Update Main.php

--- src/ShadowMikado/IdScraper/Main.php
+++ src/ShadowMikado/IdScraper/Main.php
@@ -16,15 +16,22 @@ class Main extends PluginBase implements Listener
     {
         $blockid = new Config($this->getDataFolder() . "BlockId.json", Config::JSON);
         $itemid = new Config($this->getDataFolder() . "ItemId.json", Config::JSON);
+        print(filesize($this->getDataFolder() . "BlockId.json"));
 
-        foreach (VanillaItems::getAll() as $item) {
-            $itemid->set($item->getName(), $item->getTypeId());
-            $itemid->save();
-        }
+        if (filesize($this->getDataFolder() . "BlockId.json") <= 2 && filesize($this->getDataFolder() . "ItemId.json") <= 2) {
+            foreach (VanillaItems::getAll() as $item) {
+                $itemid->set($item->getName(), $item->getTypeId());
+                $itemid->save();
+            }
 
-        foreach (VanillaBlocks::getAll() as $block) {
-            $blockid->set($block->getName(), $block->getTypeId());
-            $blockid->save();
+            foreach (VanillaBlocks::getAll() as $block) {
+                $blockid->set($block->getName(), $block->getTypeId());
+                $blockid->save();
+            }
+
+            $this->getLogger()->notice("Ids scraped in " . $this->getDataFolder());
+        } else {
+            $this->getLogger()->notice("Ids are already scraped !");
         }
     }
 }
